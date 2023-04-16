ea10ad00b9d51cae5bc51001cb3912d7b9d0b7bc
Andrew Merrin <78210129+AM1010101@users.noreply.github.com>
2023-04-16 14:31:39 +0100

    allow localhost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 from app.endpoints import health, item
 
 app = FastAPI()
@@ -7,3 +8,15 @@ app = FastAPI()
 # add endpoints
 app.include_router(health.router, tags=["health"])
 app.include_router(item.router, tags=["item"])
+
+origins = [
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
