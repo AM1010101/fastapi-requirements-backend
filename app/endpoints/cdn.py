df7e6747e900e4869c3b49cc11f265529fa55054
Andrew Merrin <78210129+AM1010101@users.noreply.github.com>
2023-04-22 17:25:35 +0100

    added loading

--- app/endpoints/cdn.py
+++ app/endpoints/cdn.py
@@ -11,3 +11,12 @@ async def get_logo():
     
     # Return the SVG with appropriate headers
     return Response(content=logo_svg, media_type="image/svg+xml")
+
+@router.get("/loading")
+async def get_loading():
+    # Fetch the SVG logo from a URL
+    response = requests.get("https://firebasestorage.googleapis.com/v0/b/fir-test-92d5e.appspot.com/o/loading.svg?alt=media&token=8b8ec4d7-4e0b-4441-a381-7b469075689c")
+    logo_svg = response.content
+    
+    # Return the SVG with appropriate headers
+    return Response(content=logo_svg, media_type="image/svg+xml")
